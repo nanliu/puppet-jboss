7ad06df169dc195062544e5572ededf0bda885aa
Nan Liu <nan@puppetlabs.com>
2012-02-16 10:59:53 -0800

    Fix source name.

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,7 +6,6 @@ class jboss::package (
   $package_name = hiera('jboss_package_name', 'jboss'),
   $version      = hiera('jboss_version', 7),
   # settings below only relevant for file deployment
-  $filename     = hiera('jboss_filename', 'apache-jboss-6.0.35.tar.gz'),
   $source       = hiera('jboss_source', 'undef'),
   $target       = hiera('jboss_target', '/opt')
 ) {
@@ -30,13 +29,13 @@ class jboss::package (
       if $source {
         $jboss_source = $source
         } else {
-          case $jboss::package::version {
+          case $version {
             '5': { $jboss_source = 'http://sourceforge.net/projects/jboss/files/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip/download' }
             '6': { $jboss_source = 'http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip' }
             '7': { $jboss_source = 'http://download.jboss.org/jbossas/7.0/jboss-as-7.0.2.Final/jboss-as-web-7.0.2.Final.zip' }
           }
         }
-        $basename = staging_parse($filename, 'basename')
+        $basename = staging_parse($jboss_source, 'basename')
 
         staging::deploy { $basename:
           source => $jboss_source,
